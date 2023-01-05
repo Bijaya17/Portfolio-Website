cea6f9f97c19fcfbd55fd7a3d665b41417cc6e72
Bijaya Mandal <87533088+Bijaya17@users.noreply.github.com>
2023-01-06 00:38:27 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -26,4 +26,4 @@ const sr = ScrollReveal ({
 sr.reveal('.home-text',{delay:350, origin:'left'})
 sr.reveal('.home-img',{delay:350, origin:'right'})
 
-sr.reveal('.sub-service,.about,.portfolio,.service,.skills,.education,.projects,.cta,.contact',{delay:200, origin:'bottom'})
+sr.reveal('.about,.education,.projects,.skills,.cta,.contact',{delay:200, origin:'bottom'})
